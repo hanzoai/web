bbaacbbba2d728775751c8392a68339b4b9d8b7a
TopTech126 <carlie197821@gmail.com>
2024-06-22 04:13:36 +1000

    modify social link by  package file fix-3

--- packages/brand/components/footer.tsx
+++ packages/brand/components/footer.tsx
@@ -102,19 +102,19 @@ const Footer: React.FC<{
                 Follow Us
               </span>
               <div className='grid grid-cols-2 gap-3 text-[16px] lg:mt-[43.1px] mt-[30px]'>
-                <Link className='' href={"http://x.com/hanzoai"}>
+                <Link className='' href={"https://x.com/hanzoai"}>
                   X
                 </Link>
-                <Link className='' href={"http://linkedin.com/company/hanzoai"}>
+                <Link className='' href={"https://linkedin.com/company/hanzoai"}>
                   Linkedin
                 </Link>
-                <Link className='' href={"http://facebook.com/hanzoinc"}>
+                <Link className='' href={"https://facebook.com/hanzoinc"}>
                   Facebook
                 </Link>
                 <Link className='' href={"https://github.com/hanzoai"}>
                   Github
                 </Link>
-                <Link className='' href={"http://instagram.com/hanzoinc"}>
+                <Link className='' href={"https://instagram.com/hanzoinc"}>
                   Instagram
                 </Link>
               </div>
