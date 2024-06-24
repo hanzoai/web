74316a68fe653bb7975e9215a3628ffc2bc12c77
zoosos <wbin90620@gmail.com>
2024-06-23 19:10:46 -0700

    fiexed logo error

--- packages/brand/components/logo.tsx
+++ packages/brand/components/logo.tsx
@@ -1,86 +1,95 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 
 import { type TShirtSize  } from '@hanzo/ui/types'
-
 import { cn } from '@hanzo/ui/util'
 
 import * as Icons from './icons'
 
-const TEXT = 'Hanzo'
-
 const Logo: React.FC<{
   size?: TShirtSize
-  variant?: 'text-only' | 'logo-only' | 'full'
-  onClick?: () => void
+  layout?: 'text-only' | 'logo-only' | 'full'
   href?: string
-  outerClx?: string
-  textClx?: string
+  className?: string
+  spanClassName?: string
 }> = ({
   size,
   href, // no default please!
-  outerClx='',
-  textClx='',
-  variant='full',
-  onClick,
+  className='',
+  spanClassName,
+  layout='full'
 }) => {
-  let classes: any = {}
-  const toAdd = (variant === 'logo-only') ? 
-    {
-      span: 'hidden',
+
+  const [classes, setClasses] = useState<{ icon: string, span: string}>({ icon: '', span: ''});
+  const [spanClasses, setSpanClasses] = useState<string>('');
+  const [outerClasses, setOuterClasses] = useState<string>('');
+
+  useEffect(() => {
+
+    let classesTemp: any = {}
+    const toAdd = (layout === 'logo-only') ? {
+      span: ' hidden',
       icon: ''
-    } 
-    :
-    (variant === 'text-only') ? 
-      {
-        span: '',
-        icon: 'hidden'
-      } 
-      : 
-      {
-        span: '',
-        icon: ''
-      }
+    } :
+    (layout === 'text-only') ? {
+      span: '',
+      icon: ' hidden'
+    } : 
+    {
+      span: '',
+      icon: ''
+    }
+  
+    if (size === 'lg' || size === 'xl' ) { // for safety
+      classesTemp.icon = 'h-10 w-10 mr-4 color-inherit' + toAdd.icon
+      classesTemp.span = 'text-3xl' + toAdd.span
+    }
+      //match for hanzo
+    else if (size === 'md') {
+      classesTemp.icon = 'h-[40.82px] w-[40.82px] mr-[12px] color-inherit' + toAdd.icon
+      classesTemp.span = 'text-[1.8rem]/[1.8rem] tracking-tighter' + toAdd.span
+    }
+    else if (size === 'sm' ) {
+      classesTemp.icon = 'h-6 w-6 mr-2 color-inherit' + toAdd.icon
+      classesTemp.span = ' text-[40px] drop-shadow-md ' + toAdd.span
+    }
+      // xs
+    else {
+      classesTemp.icon = 'h-4 w-4 mr-1 color-inherit' + toAdd.icon
+      classesTemp.span = 'text-base' + toAdd.span
+    }
+  
+    const outerClassesTemp = 'flex flex-row items-center ' + className
+    const spanClassesTemp = 'inline-block font-sans pl-[5px] ' 
+      + spanClassName
+      + (href ? ' hover:text-accent ' : ' cursor-default ') 
+      + classesTemp.span 
 
-  if (size === 'lg' || size === 'xl' ) { // for safety
-    classes.icon = 'h-10 w-10 mr-4 color-inherit ' 
-    classes.span = 'text-3xl ' 
-  }
-    // match lux.network
-  else if (size === 'md') {
-    classes.icon = 'h-[32px] w-[32px] mr-[12px] color-inherit '
-    classes.span = 'text-[1.8rem]/[1.8rem] tracking-tighter '
-  }
-  else if (size === 'sm' ) {
-    classes.icon = 'h-6 w-6 mr-2 color-inherit ' 
-    classes.span = 'text-lg '
-  }
-    // xs
-  else {
-    classes.icon = 'h-4 w-4 mr-1 color-inherit '
-    classes.span = 'text-2xl '
-  }
+      console.log(classesTemp)
 
-  classes.icon += toAdd.icon
-  classes.span += toAdd.span
+      setClasses(classesTemp)
+      setSpanClasses(spanClassesTemp)
+      setOuterClasses(outerClassesTemp)
 
+  }, [layout, size, className, spanClassName, href])
 
-  const outerClasses = 'flex flex-row items-center ' + outerClx
-  const spanClasses = 'inline-block font-bold font-heading ' 
-    + textClx
-    + (href ? ' hover:text-accent cursor-pointer ' : ' cursor-default ') 
-    + classes.span 
+  const Inner: React.FC = () => (<>
+      <Icons.logo className={classes.icon} />
+      <span className={cn(spanClasses, ' text-inherit ')}>Hanzo</span>
+  </>)
+
 
   return (
     href ? (
-      <Link href={href} className={outerClasses} onClick={onClick} >
-        <Icons.logo className={classes.icon} />
-        <span className={spanClasses}>{TEXT}</span>
+      <Link href={href} className={outerClasses} >
+        <Inner />
       </Link>
+  
     ) : (
-      <span className={outerClasses} onClick={onClick}>
-        <Icons.logo className={classes.icon} />
-        <span className={spanClasses}>{TEXT}</span>
+      <span className={outerClasses} >
+        <Inner />
       </span>
     )
   )
