3768cc86b0fe309f8b2bc00b25b9cf0b3dbe3259
toptech126 <carlie197821@gmail.com>
2024-06-30 23:01:18 +0800

    update: 404 page

--- packages/brand/components/not-found/notFound.tsx
+++ packages/brand/components/not-found/notFound.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import React from 'react';
 
-export function ErrorPage() {
+const ErrorPage: React.FC = () => {
   return (
-    <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-black text-[white] font-inter">
+    <div className="flex flex-col items-center justify-center bg-black text-white">
       <div className="mx-auto max-w-md text-center">
         <h1 className="text-[90px] font-bold text-[white]">404</h1>
         <p className="mt-4 text-2xl text-[white]">Oops, page not found</p>
         <Link
           href="/"
-          className="mt-6 inline-flex items-center rounded-[5px] bg-[white] px-4 py-2 text-sm font-medium text-[black] shadow-sm transition-colors hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
+          className="mt-6 inline-flex items-center rounded-[5px] bg-[white] px-4 py-2 text-sm font-medium !text-[black] shadow-sm transition-colors hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
           prefetch={false}
         >
           Go to Homepage
@@ -18,3 +18,5 @@ export function ErrorPage() {
     </div>
   )
 }
+
+export default ErrorPage;
