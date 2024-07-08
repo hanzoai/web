73a5716a8d311823bd691337b58a807f6f4b87cd
duongchristopher <duongchristopher@outlook.com>
2024-07-08 13:51:00 +0900

    fix video issue

---
+++ sites/landing/src/content/slides/details/video/solutions.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { type LucideProps } from 'lucide-react'
+
+const Solutions: React.FC<LucideProps> = (props: LucideProps) => (
+    <source src='/assets/hanzo-site-animation/solutions.mp4' type='video/mp4'></source>
+)
+
+export default Solutions
