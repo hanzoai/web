6e300834794b7fc604714e01d22c6a33b363b19c
unknown <musordmt@proton.me>
2024-07-19 20:09:35 +0300

    update

--- app/dash/src/layout/header.tsx
+++ app/dash/src/layout/header.tsx
@@ -20,11 +20,10 @@ const AdminHeader: React.FC<{
 
     useEffect(() => {
       const handleCommandMenuEvent = (event: KeyboardEvent) => {
-        if ((event.metaKey || event.ctrlKey) && event.key == 'k') 
-          {
-            event.preventDefault()
-            setOpenCommandMenu(true)
-          }
+        if ((event.metaKey || event.ctrlKey) && event.key == 'k') {
+          event.preventDefault()
+          setOpenCommandMenu(true)
+        }
       }
       document.addEventListener("keydown", (event) => handleCommandMenuEvent(event))
 
@@ -60,11 +59,11 @@ const AdminHeader: React.FC<{
               <Button
                 variant="outline"
                 size='icon'
-                className='rounded-full text-muted border-2 border-muted bg-level-1 hover:bg-level-2 hover:border-foreground hover:text-foreground uppercase w-8 h-8'
+                className='rounded-full text-muted bg-level-1 hover:bg-level-2 hover:border-foreground hover:text-foreground uppercase w-8 h-8'
               ><img src="assets/images/01.png" className="aspect-square rounded-full" alt="profile" width={30} /></Button>
             </PopoverTrigger>
-            <PopoverContent className='bg-level-0 mr-4 !px-0'>
-              <div className="grid gap-4">
+            <PopoverContent className='bg-level-0 mr-8 !px-0 text-sm text-primary font-light border-level-1'>
+              <div className="grid gap-2">
                 <div className="space-y-2 truncate px-4">
                   {auth.user?.displayName ? (
                     <>
@@ -82,23 +81,29 @@ const AdminHeader: React.FC<{
                     </Button>
                   )}
                 </div>
-                <Separator />
-                    <div className='h-8 flex flex-row justify-between text-sm opacity-50 truncate rounded-sm hover:bg-muted-4 hover:cursor-pointer items-center mx-2 px-2' onClick={() => setOpenCommandMenu(true)}>
-                      <div className=''>Command Menu</div>
-                      <div className='flex flex-row gap-2 text-xs'>
-                        <div className='rounded-sm border p-1 bg-background'>Ctrl</div>
-                        <div className='rounded-sm border p-1 bg-background'>K</div>
-                      </div>
-                    </div>
-                <Separator />
+                <Separator className='bg-level-1'/>
+                <div className='flex flex-col'>
+                  <div className='h-8 flex flex-row justify-between truncate rounded-sm hover:bg-level-1 hover:cursor-pointer items-center mx-2 px-2' onClick={() => setOpenCommandMenu(true)}>Dashboard</div>
+                  <div className='h-8 flex flex-row justify-between truncate rounded-sm hover:bg-level-1 hover:cursor-pointer items-center mx-2 px-2' onClick={() => setOpenCommandMenu(true)}>Dashboard</div>
+                  <div className='h-8 flex flex-row justify-between truncate rounded-sm hover:bg-level-1 hover:cursor-pointer items-center mx-2 px-2' onClick={() => setOpenCommandMenu(true)}>Dashboard</div>
+                </div>
+                <Separator className='bg-level-1'/>
+                <div className='h-8 flex flex-row justify-between truncate rounded-sm hover:bg-level-1 hover:cursor-pointer items-center mx-2 px-2' onClick={() => setOpenCommandMenu(true)}>
+                  <p>Command Menu</p>
+                  <div className='flex flex-row gap-2 text-xs'>
+                    <div className='rounded-sm border border-level-2 p-1 bg-background'>Ctrl</div>
+                    <div className='rounded-sm border border-level-2 p-1 bg-background'>K</div>
+                  </div>
+                </div>
+                <Separator className='bg-level-1' />
                 <div className='px-2'>
-                  <Button variant="outline" onClick={() => handleLogOut} className='w-full'>Logout</Button>
+                  <Button variant="primary" onClick={() => handleLogOut} className='w-full'>Logout</Button>
                 </div>
               </div>
             </PopoverContent>
           </Popover>
         </div>
-        <ModalDialog open={openCommandMenu} setOpen={setOpenCommandMenu}/>
+        <ModalDialog open={openCommandMenu} setOpen={setOpenCommandMenu} />
       </div>
     )
   }
