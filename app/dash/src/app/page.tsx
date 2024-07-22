2063b740238a7d97317946f346e89c1cff9d618d
unknown <musordmt@proton.me>
2024-07-23 00:28:06 +0300

    hidden sidebar

--- app/dash/src/app/page.tsx
+++ app/dash/src/app/page.tsx
@@ -8,25 +8,25 @@ const UniversalPage = () => {
   const router = useRouter();
   const auth = useAuth()
 
-  // router.push("/dashboard")
+  router.push("/dashboard")
 
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_LOGIN_SITE_URL}/api/auth/get-auth-token`, {
-      method: 'GET',
-      credentials: 'include',
-    })
-    .then(response => response.json())
-    .then(async (data: any) => {
-      const token = data.reqToken
-      if (!!token) {
-        await auth.loginWithCustomToken(token)
-        console.log(auth.loggedIn)
+  // useEffect(() => {
+  //   fetch(`${process.env.NEXT_PUBLIC_LOGIN_SITE_URL}/api/auth/get-auth-token`, {
+  //     method: 'GET',
+  //     credentials: 'include',
+  //   })
+  //   .then(response => response.json())
+  //   .then(async (data: any) => {
+  //     const token = data.reqToken
+  //     if (!!token) {
+  //       await auth.loginWithCustomToken(token)
+  //       console.log(auth.loggedIn)
 
-        if (auth.loggedIn) router.push("/dashboard")
-        else router.push('https://auth.hanzo.ai/?redirectUrl=https://dash.hanzo.ai')
-      }
-    })
-  }, [auth])
+  //       if (auth.loggedIn) router.push("/dashboard")
+  //       else router.push('https://auth.hanzo.ai/?redirectUrl=https://dash.hanzo.ai')
+  //     }
+  //   })
+  // }, [auth])
 }
 
 export default UniversalPage
