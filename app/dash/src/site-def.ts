59d6c11bd6bae6307c70b99868f52e2f74297674
unknown <musordmt@proton.me>
2024-07-25 18:10:25 +0300

    bug fix

--- app/dash/src/site-def.ts
+++ app/dash/src/site-def.ts
@@ -1,5 +1,4 @@
 import { footer, mainNav, type SiteDef  } from '@hanzo/brand/site-def'
-import { commerceConfig as commerce } from '@hanzo/data/commerce'
 
 export default {
   currentAs: 'https://dash.hanzo.ai',
