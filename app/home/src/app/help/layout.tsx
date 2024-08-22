3c8fbf8b195d03eb062dd918b0a95ba27cd9aeff
unknown <musordmt@proton.me>
2024-08-22 23:14:10 +0300

    move helpCenter component to src

--- app/home/src/app/help/layout.tsx
+++ app/home/src/app/help/layout.tsx
@@ -5,11 +5,10 @@ import { Header, Footer } from '@hanzo/brand'
 import siteDef from '@/site-def'
 
 const Layout: React.FC<PropsWithChildren> = async ({
-  children
+    children
 }) => (<>
-  <Header siteDef={siteDef}/>
     {children}
-  <Footer siteDef={siteDef} className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
+    <Footer siteDef={siteDef} className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
 </>)
 
 export default Layout
--- app/home/src/app/help/page.tsx
+++ app/home/src/app/help/page.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 
-import { Header } from "@hanzo/brand";
+import { HelpCenter } from "@/components/component/helpCenter"
 
-import siteDef from "@/site-def";
-import "@/blocks/registerComponents";
-
-type Props = {
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
-
-import {HelpCenter} from "../../../components/component/helpCenter"
-
-const page = ({}: Props) => {
-   return (
-    <>
-      <Header siteDef={siteDef} />
-      <HelpCenter />
-      {/* <FooterSlide agent={agent} /> */}
-    </>
+const page = () => {
+  return (
+    <HelpCenter />
   );
 };
 export default page;
